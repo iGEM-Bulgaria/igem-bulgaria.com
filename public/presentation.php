fc0210993b5457a8a6ffd8d4a21ca92600919c68
Krasiyan <hello@krasiyan.com>
2016-04-19 19:06:34 +0300

    added presentation page

---
+++ public/presentation.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>iGEM България</title>
+    <meta name="description" content="" >
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+
+    <!-- Bootstrap  -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- icon fonts font Awesome -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom Styles -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/presentation.css" rel="stylesheet">
+    <link href="assets/css/jquery.flipster.min.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+    <script src="assets/js/html5shiv.js"></script>
+    <![endif]-->
+
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/images/favicons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/images/favicons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/images/favicons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/images/favicons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/images/favicons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/images/favicons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/images/favicons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/images/favicons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/images/favicons/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="assets/images/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="assets/images/favicons/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="assets/images/favicons/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="assets/images/favicons/favicon-16x16.png" sizes="16x16">
+    <link rel="manifest" href="assets/images/favicons/manifest.json">
+    <link rel="mask-icon" href="assets/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="assets/images/favicons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#2d89ef">
+    <meta name="msapplication-TileImage" content="assets/images/favicons/mstile-144x144.png">
+    <meta name="msapplication-config" content="assets/images/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-76150554-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+  </head>
+  <body>
+
+    <!-- Preloader -->
+    <div id="preloader">
+      <div id="loader">
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="lading"></div>
+      </div>
+    </div><!-- /#preloader -->
+    <!-- Preloader End-->
+
+    <section id="page-top" class="section-style" data-background-image="assets/images/background/top.jpg">
+      <div class="pattern height-resize">
+        <div class="container-fluid">
+          <div class="col-md-6">
+            <h1 class="text-center">Какво е iGEM?</h1>
+            <p class="text-justify">
+              Международното състезание по генно инженерство iGEM (international genetically engineered machines) се провежда всяка година в Бостън. То позволява на млади студенти (до 23 години) да представят своите проекти и да участват в състезанието с тях. IGEM е комбинация от лабораторни умения, научна разработка, финансово планиране и мениджмънт, изготвяне на цялостен обществено значим научен проект и неговото изпълнение.
+            </p>
+
+            <h1 class="text-center">Защо искаме да участваме?</h1>
+            <p>
+            С участието си в състезанието на iGEM ние целим няколко основни неща:<br/>
+              <ul>
+                <li>Да утвърдим позицията на България на световната сцена;</li>
+                <li>Да поставим основите на синтетичната биология у нас;</li>
+                <li>Да покажем, че в България има будни млади хора, които се интересуват от наука и които вярват, че тук може да се постигнат успехи на световно ниво</li>
+                <li>Да докажем, че инвестицията в науката е възвръщаема.</li>
+              </ul>
+            </p>
+
+
+            <h1 class="text-left">Дарения чрез банкова транзакция:</h1>
+            <p>
+              АЙ ДЖИ И ЕМ БЪЛГАРИЯ<br/><br/>
+
+              Първа инвестиционна банка АД<br/>
+              бул. "Драган Цанков" №37<br/>
+              <strong>BIC: FINVBGSF</strong><br/>
+              <strong>IBAN - BGN: BG35FINV91501216732479</strong><br/>
+              <strong>IBAN - USD: BG12FINV91501216732505</strong><br/><br/>
+
+              <strong>N.B. Отворени сме за сключване на договори за спонсорство и дарение.</strong>
+            </p>
+          </div>
+          <div class="col-md-6">
+            <video controls>
+              <source src="http://media12.vbox7.com/s/86/860ab56896rc88c545e1.mp4" type="video/mp4">
+              <iframe width="560" height="315" src="http://vbox7.com/emb/external.php?vid=860ab56896" frameborder="0" allowfullscreen></iframe>
+            </video>
+
+            <div class="clearfix"></div>
+
+            <video controls>
+              <source src="http://cdn1.bitelevision.com/upload/2016/04/08/SINTENTICHNA_BIOLOGIA.mp4?_=1" type="video/mp4">
+            </video>
+
+            <div class="clearfix"></div>
+
+            <img id="bnr" src="http://www.predavatel.com/bg/radio/bnr_images/bnr_radio-bulgaria.png">
+            <div class="clearfix"></div>
+            <audio controls>
+              <source src="http://82.119.84.123/storage/Sofia/RadioCafe/120416_zlati.mp3" type="audio/mpeg">
+            </audio>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <!-- jQuery Library -->
+    <script type="text/javascript" src="assets/js/jquery-2.1.0.min.js"></script>
+    <!-- Modernizr js -->
+    <script type="text/javascript" src="assets/js/modernizr-2.8.0.min.js"></script>
+    <!-- Plugins -->
+    <script type="text/javascript" src="assets/js/plugins.js"></script>
+    <!-- Custom JavaScript Functions -->
+    <script type="text/javascript" src="assets/js/functions.js"></script>
+    <!-- Custom JavaScript Functions -->
+    <script type="text/javascript" src="assets/js/circle-progress.js"></script>
+    <!-- Slider Javascript Functions -->
+    <script src="assets/js/jquery.flipster.min.js"></script>
+  </body>
+</html>
